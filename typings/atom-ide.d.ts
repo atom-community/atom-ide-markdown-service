4e7f229e1555a2a897c21aca3b7cd0ece8bbadab
Andreas Gerlach <andreas.gerlach@xarvio.com>
2019-04-18 16:11:45 +0200

    feat: typing and documentation added

---
+++ typings/atom-ide.d.ts
@@ -0,0 +1,7 @@
+// @ts-check
+/// <reference path="../node_modules/atom-languageclient/typings/atom-ide/index.d.ts" />
+/// <reference path="./atom-ide-community.d.ts" />
+
+import * as AtomIDE from 'atom-ide';
+export = AtomIDE;
+export as namespace AtomIDE;
