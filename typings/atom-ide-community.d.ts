b41a252bd293617f7259057f5c2b2c9f88ef3d5a
aminya <aminyahyaabadi74@gmail.com>
2020-07-17 08:58:45 -0500

    chore: fix type

--- typings/atom-ide-community.d.ts
+++ typings/atom-ide-community.d.ts
@@ -3,7 +3,7 @@ declare module "atom-ide" {
    * the markdown service object
    * @type {AtomIDE.MarkdownService}
    */
-  export declare type render = (
+  export type render = (
     markdownText: string,
     grammar: string
   ) => Promise<string>;
