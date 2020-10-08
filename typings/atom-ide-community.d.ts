41595fb23d4c81f823bceb473e9baf5dacd559bb
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-10-08 02:17:15 -0500

    chore: remove typings

--- typings/atom-ide-community.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "atom-ide" {
-  /**
-   * the markdown service object
-   * @type {AtomIDE.MarkdownService}
-   */
-  export type render = (markdownText: string, grammar: string) => Promise<string>
-}
